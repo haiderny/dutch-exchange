0ebfea7a54dbfb16c8afa52817182f0919132dca
Velenir <Velenir@users.noreply.github.com>
2017-11-03 17:03:58 +0300

    deploy multiple Token contracts and save to config

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,50 @@
-var DutchExchange = artifacts.require("./DutchExchange.sol");
-var DutchExchangeFactory = artifacts.require("./DutchExchangeFactory.sol");
-var Token = artifacts.require("./Token.sol");
+const DutchExchange = artifacts.require('./DutchExchange.sol')
+const DutchExchangeFactory = artifacts.require('./DutchExchangeFactory.sol')
+const Token = artifacts.require('./Token.sol')
 
-module.exports = function(deployer) {
-  deployer.deploy(DutchExchange);
-  deployer.deploy(DutchExchangeFactory);
-  deployer.deploy(Token, {});
-};
+const fs = require('fs')
 
-// TODO
-// fake issuance of tokens
+const code2tokenMap = {
+  ETH: 'ETHER',
+  GNO: 'GNOSIS',
+  REP: 'AUGUR',
+  '1ST': 'FIRST BLOOD',
+  OMG: 'OMISEGO',
+  GNT: 'GOLEM',
+}
+
+const codeList = Object.keys(code2tokenMap)
+
+module.exports = async function f(deployer) {
+  deployer.deploy(DutchExchange)
+  deployer.deploy(DutchExchangeFactory)
+  deployer.deploy(Token, {})
+
+  // console.log('New token at', (await Token.new()).address)
+  // console.log('New token at', (await Token.new()).address)
+  // console.log('New token at', (await Token.new()).address)x
+
+  // deploy Token contracts to testrpc
+  const promisedTokens = Array.from({ length: codeList.length }, () => Token.new())
+  // wait for the transactions
+  const tokenContracts = await Promise.all(promisedTokens)
+
+  /**
+   * each token gets assigned an arbitrary address
+   * {
+   *  ETH: '0xfds65sd767sfd6',
+   *  GNO: '0xy34r34ytrg3u4r',
+   *  ...
+   * }
+   */
+  const code2AddresstMap = tokenContracts.reduce((acc, { address }, i) => {
+    acc[codeList[i]] = address
+    return acc
+  }, {})
+
+  // write to file to later be imported in App
+  fs.writeFile('./build/code2Address.json', JSON.stringify(code2AddresstMap, null, '\n'), (err) => {
+    if (err) console.error(err)
+    console.log('Token addresses written to build/code2Address.json')
+  })
+}
